4adc5f8cf9c2439b786afeb71b5ebe21796a30a3
Igor Cardoso <iygorsilva@gmail.com>
2022-02-28 18:32:46 -0300

    controller and module product, and database settings

--- product-store/src/app.module.ts
+++ product-store/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test_product_store',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
