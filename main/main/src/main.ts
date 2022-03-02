ec60d37f717f3a2be01b8c513f7d49876995fed2
Igor Cardoso <iygorsilva@gmail.com>
2022-03-02 00:48:45 -0300

    microservice settings with rabbitmq docker

--- main/main/src/main.ts
+++ main/main/src/main.ts
@@ -1,14 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:4200'
+  // app.setGlobalPrefix('api');
+  // app.enableCors({
+  //   origin: 'http://localhost:4200'
+  // });
+
+  // await app.listen(3001);
+
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['http://localhost:5672'],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
   });
 
-  await app.listen(3001);
+  app.listen();
+
 }
 bootstrap();
