37b20f2c45560b7dc29b56b0c7f0c921fb50bec9
Igor Cardoso <iygorsilva@gmail.com>
2022-03-07 01:29:43 -0300

    settings rabbitmq   -   main

--- main/main/src/main.ts
+++ main/main/src/main.ts
@@ -3,27 +3,26 @@ import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix('api');
+  // const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api');
   // app.enableCors({
   //   origin: 'http://localhost:4200'
   // });
 
-  await app.listen(3001);
+  // await app.listen(3001);
 
-  // const app = await NestFactory.createMicroservice(AppModule, {
-  //   transport: Transport.RMQ,
-  //   options: {
-  //     urls: ['http://localhost:5672'],
-  //     queue: 'main_queue',
-  //     queueOptions: {
-  //       durable: false,
-  //     },
-  //   },
-  // });
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqps://oulvbner:qrtnF2MPL4rdbo0GgRxC-nC_JWUcVCmh@chimpanzee.rmq.cloudamqp.com/oulvbner'],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
 
-  // app.listen();
+  app.listen();
 
 }
 bootstrap();
