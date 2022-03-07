36e835de8c5156b6aceb86f833300ef8e4857f25
Igor Cardoso <iygorsilva@gmail.com>
2022-03-07 01:28:42 -0300

    settings rabbitmq    -    product-store

--- product-store/src/modules/product/product.module.ts
+++ product-store/src/modules/product/product.module.ts
@@ -3,10 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Product])
+    TypeOrmModule.forFeature([Product]),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqps://oulvbner:qrtnF2MPL4rdbo0GgRxC-nC_JWUcVCmh@chimpanzee.rmq.cloudamqp.com/oulvbner'],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ]),
   ],
   controllers: [ProductController],
   providers: [ProductService]
