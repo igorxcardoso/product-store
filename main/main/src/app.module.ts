73906dd22d67de0d85dbb04591abc585377ff4d4
Igor Cardoso <iygorsilva@gmail.com>
2022-03-06 23:49:19 -0300

    mongo settings

--- main/main/src/app.module.ts
+++ main/main/src/app.module.ts
@@ -6,7 +6,7 @@ import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
+    MongooseModule.forRoot('mongodb://localhost:27017/nest_main', {
       autoCreate: true
     }),
     ProductModule
--- main/main/src/main.ts
+++ main/main/src/main.ts
@@ -3,27 +3,31 @@ import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-  // app.setGlobalPrefix('api');
-  // app.enableCors({
-  //   origin: 'http://localhost:4200'
-  // });
-
-  // await app.listen(3001);
-
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: ['http://localhost:5672'],
-      queue: 'main_queue',
-      queueOptions: {
-        durable: false,
-      },
-    },
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:4200'
   });
 
-  app.listen();
+  await app.listen(3001);
+
+
+
+
+
+  // const app = await NestFactory.createMicroservice(AppModule, {
+  //   transport: Transport.RMQ,
+  //   options: {
+  //     urls: ['http://localhost:5672'],
+  //     queue: 'main_queue',
+  //     queueOptions: {
+  //       durable: false,
+  //     },
+  //   },
+  // });
+
+  // app.listen();
 
 }
 bootstrap();
