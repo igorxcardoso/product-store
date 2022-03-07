77cace3ecfecbcb560e9bd158ddec7a0ac72a8bd
Igor Cardoso <iygorsilva@gmail.com>
2022-03-07 00:27:58 -0300

    mongo update docker and test

--- main/main/src/main.ts
+++ main/main/src/main.ts
@@ -6,15 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:4200'
-  });
+  // app.enableCors({
+  //   origin: 'http://localhost:4200'
+  // });
 
   await app.listen(3001);
-
-
-
-
 
   // const app = await NestFactory.createMicroservice(AppModule, {
   //   transport: Transport.RMQ,
